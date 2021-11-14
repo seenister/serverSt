1fecae1f3ae3d965fcf1c9481ab6215ebfaab8ff
Anastasia Usoltseva <neeeerk@yandex.ru>
2021-11-14 15:24:05 +0300

    v2.1

---
+++ src/main/java/servlets/MirrorRequestsServlet.java
@@ -0,0 +1,19 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MirrorRequestsServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+
+        response.getWriter().println(request.getParameter("key").toString());
+
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+}
