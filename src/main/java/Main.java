ad18af61aa064e8ec9287ad9214c3dd3f24cf350
Anastasia Usoltseva <neeeerk@yandex.ru>
2021-12-18 16:35:31 +0300

    add WebSockets

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,18 @@
 package main;
 
-import accounts.AccountService;
-import org.eclipse.jetty.server.Handler;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.handler.ResourceHandler;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import servlets.SignInServlet;
-import servlets.SignUpServlet;
+import accounts.UserHuuzer;
+import services.UserService;
 
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        AccountService accountService = new AccountService();
 
 
+        UserService userService = new UserService();
+        userService.saveUser( new UserHuuzer("Some", "One1"));
+
+/*
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(new ServletHolder(new SignUpServlet(accountService)), "/signup");
         context.addServlet(new ServletHolder(new SignInServlet(accountService)), "/signin");
@@ -34,5 +30,6 @@ public class Main {
         System.out.println("Server started");
         server.join();
 
+    */
     }
 }
