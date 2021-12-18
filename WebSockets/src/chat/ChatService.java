cf46379f538a866d44c240c8aca54856717dda1d
Anastasia Usoltseva <neeeerk@yandex.ru>
2021-12-18 22:28:23 +0300

    update chat

--- WebSockets/src/chat/ChatService.java
+++ WebSockets/src/chat/ChatService.java
@@ -1,31 +1,42 @@
 package chat;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author v.chibrikov
- *         <p>
- *         Пример кода для курса на https://stepic.org/
- *         <p>
- *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
+ * <p>
+ * Пример кода для курса на https://stepic.org/
+ * <p>
+ * Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
  */
 public class ChatService {
     private Set<ChatWebSocket> webSockets;
+    Set<String> banList = new HashSet<>();
+
 
     public ChatService() {
         this.webSockets = Collections.newSetFromMap(new ConcurrentHashMap<>());
     }
 
     public void sendMessage(String data) {
+        String[] subStr = data.split(":", 2);
+        String metBan = "/ban";
         for (ChatWebSocket user : webSockets) {
             try {
-                user.sendString(data);
+                if (banList.contains(subStr[0])) {
+                    System.out.println("you are banned!");
+                }else {  user.sendString(data);}
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
+        if (subStr[1].startsWith(metBan)) {
+            banList.add(subStr[1].substring(metBan.length()));
+        }
+
     }
 
     public void add(ChatWebSocket webSocket) {
